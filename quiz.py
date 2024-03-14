bfe6fd6aedbba9ef3d460ff4d7ba14dbfc60ec80
Aiyana246 <98262198+Aiyana246@users.noreply.github.com>
2024-03-13 23:11:45 -0400

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,68 @@
+print("Welcome to Barbados Quiz")
+
+playing = input("Are you ready to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay!")
+
+answer = input("How big is Barbados? (in mi^2) ")
+if answer == "166":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("On what day is Independence celebrated (Month Day) ")
+if answer == "November 30`":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("What kind of bird is on Barbados' Coast of Arms? ")
+if answer == "pelican":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("what is the National Flower Barbados? ")
+if answer == "pride of barbados":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("What is the official language of Barbados? ")
+if answer == "english":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("What is the capital of Barbados? ")
+if answer == "Bridgetown":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("How many parishes does Barbados have? ")
+if answer == "11":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("What is the National Bird of Barbados? ")
+if answer == "pelican":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("What is the offical motto of Barbados? ")
+if answer == "pride and industry":
+    print('Correct!')
+else: 
+    print('Incorrect')
+
+answer = input("What is the official religion of Barbados? ")
+if answer == "christianity":
+    print('Correct!')
+else: 
+    print('Incorrect')
